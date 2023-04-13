4a56a0671b12261b5bd3a4f417a357af9c8a158e
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-04-13 13:50:52 +0530

    changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs');
 
 mongoose.Promise = global.Promise;
 const connectionParams = {
-    useMongoClient: true
+    
 }
 mongoose.connect(config.database, connectionParams)
     .then(() => {
