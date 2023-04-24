1ad2cef489d6afe1b089e11e57166a9eee590d75
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-04-24 15:55:40 +0530

    dsdfsd

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.set('view engine', 'ejs');
 
 mongoose.Promise = global.Promise;
 const connectionParams = {
-    
+    // useNewUrlParser: true,
+    //   useUnifiedTopology: true,
 }
 mongoose.connect(config.database, connectionParams)
     .then(() => {
